e1cc514a8c2468d2c4f1ea96fe450b7fe8033527
Josh Hooks <josh.hooks@outlook.com>
2017-10-07 21:23:50 -0400

    move html to different class

--- views/index.jsx
+++ views/index.jsx
@@ -4,30 +4,37 @@ import Banner from './components/banner'
 import ButtonBox from './components/buttonbox'
 import {body} from './components/styles/style'
 
-export default class TestApp extends Component{
+export default class TestApp extends Component {
   render() {
     let data = this.props.data.map((o) => {
       return <Box status={o.status} desc={o.description} key={o.key}></Box>
-    });
+    })
     return (
       <html>
         <head>
-        <title>
-          TestApp
-        </title>
-      </head>
+          <title>
+            TestApp
+          </title>
+        </head>
         <body style={body}>
-          <div>
-            <Banner title="Test Bar 1" />
-            <Banner title="Test Bar 2" />
-            <ButtonBox />
-            <ButtonBox />
-            {data}
-            <Banner title="Test Bar 3" />
-          </div>
+          <Content data={data}/>
         </body>
       </html>
+    )
+  }
+}
 
+class Content extends Component {
+  render() {
+    return (
+      <div>
+        <Banner title="Test Bar 1" />
+        <Banner title="Test Bar 2" />
+        <ButtonBox />
+        <ButtonBox />
+        {this.props.data}
+        <Banner title="Test Bar 3" />
+      </div>
     )
   }
 }
